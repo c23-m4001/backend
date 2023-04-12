4cffe8d0acdbfccb67915ff3f2cd664e0b048ad5
David Yappeter <davidyap11les@gmail.com>
2023-04-12 04:59:34 +0000

    feat: config add more detailed data

--- config/config.go
+++ config/config.go
@@ -1,5 +1,18 @@
 package config
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
 type DatabaseConfig struct {
 	Host         string `yaml:"host"`
 	Port         uint16 `yaml:"port"`
@@ -9,17 +22,82 @@ type DatabaseConfig struct {
 }
 
 type YamlConfig struct {
-	BaseDir        string
-	Environment    string         `yaml:"environment"`
+	timeLocation *time.Location
+	BaseDir      string
+	StorageDir   string
+
 	DatabaseConfig DatabaseConfig `yaml:"database"`
+	Environment    string         `yaml:"environment"`
+	IsDebug        bool           `yaml:"debug"`
+	Timezone       string         `yaml:"timezone"`
 }
 
 var config YamlConfig
-
-func init() {
-
-}
+var configOnce sync.Once
 
 func GetConfig() YamlConfig {
+	configOnce.Do(func() {
+		if err := LoadConfig(); err != nil {
+			panic(err)
+		}
+	})
+
 	return config
+}
+
+func LoadConfig() error {
+	baseDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(fmt.Sprintf("%s/conf.yml", baseDir)); err != nil {
+		_, filename, _, _ := runtime.Caller(0)
+		baseDir = path.Join(path.Dir(filename), "../")
+	}
+
+	config.BaseDir = strings.TrimRight(strings.ReplaceAll(baseDir, "\\\\", "/"), "/")
+	config.StorageDir = fmt.Sprintf("%s/storage", config.BaseDir)
+
+	yamlFilePath := fmt.Sprintf("%s/conf.yml", config.BaseDir)
+	if _, err := os.Stat(yamlFilePath); err != nil {
+		return err
+	}
+
+	yamlFile, err := ioutil.ReadFile(yamlFilePath)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.UnmarshalStrict(yamlFile, &config)
+	if err != nil {
+		return err
+	}
+
+	config.timeLocation, err = time.LoadLocation(config.Timezone)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetBaseDir() string {
+	return GetConfig().BaseDir
+}
+
+func GetStorageDir() string {
+	return GetConfig().StorageDir
+}
+
+func GetJwtPrivateKeyFilePath() string {
+	return fmt.Sprintf("%s/jwt/private.key", GetStorageDir())
+}
+
+func GetJwtPublicKeyFilePath() string {
+	return fmt.Sprintf("%s/jwt/public.key", GetStorageDir())
+}
+
+func GetJwtGitIgnoreFilePath() string {
+	return fmt.Sprintf("%s/jwt/.gitignore", GetStorageDir())
 }
