65ec0b6c423b8d724afcbcae67bf71b023b876d3
David Yappeter <davidyap11les@gmail.com>
2023-04-17 09:38:24 +0000

    feat: add basic use_case panic handler

--- use_case/use_case.go
+++ use_case/use_case.go
@@ -1,1 +1,34 @@
 package use_case
+
+import (
+	"capstone/constant"
+	"capstone/delivery/dto_response"
+)
+
+var (
+	panicIsPath    = true
+	panicIsNotPath = false
+)
+
+func panicIfErr(err error, excludedErrs ...error) {
+	if err != nil {
+		for _, excludedErr := range excludedErrs {
+			if err == excludedErr {
+				return
+			}
+		}
+		panic(err)
+	}
+}
+
+func panicIfRepositoryError(err error, message string, isPath bool) {
+	if err != nil {
+		if err == constant.ErrNoData {
+			if isPath {
+				panic(dto_response.NewNotFoundResponse(message))
+			}
+			panic(dto_response.NewBadRequestResponse(message))
+		}
+		panic(err)
+	}
+}
