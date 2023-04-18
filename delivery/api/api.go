be91325b1017fea064aa47488b1f00a29c65a5fc
David Yappeter <davidyap11les@gmail.com>
2023-04-18 10:17:49 +0000

    feat: add basic api template file

--- delivery/api/api.go
+++ delivery/api/api.go
@@ -1,1 +1,135 @@
 package api
+
+import (
+	"capstone/config"
+	"capstone/delivery/dto_response"
+	"capstone/manager"
+	"capstone/util"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/iancoleman/strcase"
+)
+
+type apiContext struct {
+	ginCtx *gin.Context
+}
+
+func (a *apiContext) context() context.Context {
+	return a.ginCtx.Request.Context()
+}
+
+func (a *apiContext) getClientIp() string {
+	return a.ginCtx.ClientIP()
+}
+
+func (a *apiContext) getParam(key string) string {
+	return a.ginCtx.Param(key)
+}
+
+func (a *apiContext) getUuidParam(key string) string {
+	uuidParam := a.getParam(key)
+
+	if !util.IsUuid(uuidParam) {
+		panic(dto_response.NewBadRequestResponse(fmt.Sprintf("%s must be a valid UUID", strcase.ToCamel(key))))
+	}
+
+	return uuidParam
+}
+
+func (a *apiContext) shouldBind(obj interface{}) error {
+	return util.ShouldGinBind(a.ginCtx, obj)
+}
+
+func (a *apiContext) mustBind(obj interface{}) {
+	if err := a.shouldBind(obj); err != nil {
+		panic(a.translateBindErr(err))
+	}
+}
+
+func (a *apiContext) translateBindErr(err error) dto_response.ErrorResponse {
+	var r dto_response.ErrorResponse
+
+	switch v := err.(type) {
+	case *json.UnmarshalTypeError:
+		r = dto_response.NewBadRequestResponse("Invalid request payload (type error)")
+
+	case *json.InvalidUnmarshalError:
+		r = dto_response.NewBadRequestResponse("Invalid request payload (unmarshal error)")
+
+	default:
+		switch v {
+		case binding.ErrConvertMapStringSlice,
+			binding.ErrConvertToMapString,
+			binding.ErrMultiFileHeader,
+			binding.ErrMultiFileHeaderLenInvalid:
+			r = dto_response.NewBadRequestResponse("Invalid request payload (unmarshal error)")
+
+		default:
+			panic(err)
+		}
+	}
+
+	return r
+}
+
+func (a *apiContext) json(code int, obj interface{}) {
+	a.ginCtx.JSON(code, obj)
+}
+
+func registerMiddlewares(router gin.IRouter, container *manager.Container) {
+
+}
+
+func registerRoutes(router gin.IRouter, useCasemanager manager.UseCaseManager) {
+
+}
+
+func NewRouter(container *manager.Container) *gin.Engine {
+	allowedHeaders := []string{
+		"Accept",
+		"Accept-Encoding",
+		"Authorization",
+		"Cache-Control",
+		"Content-Type",
+		"Content-Length",
+		"Origin",
+		"X-CSRF-Token",
+		"X-Requested-With",
+	}
+
+	if config.IsProduction() {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.New()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: config.GetConfig().CorsAllowedOrigins,
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodDelete,
+			http.MethodPatch,
+			http.MethodHead,
+		},
+		AllowHeaders: allowedHeaders,
+		ExposeHeaders: []string{
+			"Content-Length",
+		},
+		AllowCredentials: true,
+		MaxAge:           2 * time.Hour,
+	}))
+
+	registerMiddlewares(router, container)
+
+	registerRoutes(router, container.UseCaseManager())
+
+	return router
+}
