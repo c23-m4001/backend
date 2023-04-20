32d663b2ecdfab743e9c46250bbbf2df4935a650
David Yappeter <davidyap11les@gmail.com>
2023-04-20 09:29:00 +0000

    feat: fill main command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"capstone/config"
+	"capstone/delivery/api"
+	"capstone/manager"
 	"fmt"
 	"os"
 
@@ -12,7 +15,20 @@ var rootCmd = &cobra.Command{
 	Short: "Run server",
 	Long:  `Run backend server`,
 	Run: func(cmd *cobra.Command, args []string) {
+		container := manager.NewContainer(manager.LoadDefault)
+		defer func() {
+			if err := container.Close(); err != nil {
+				panic(err)
+			}
+		}()
 
+		router := api.NewRouter(container)
+
+		addr := fmt.Sprintf(":%d", config.GetConfig().Port)
+		err := router.Run(addr)
+		if err != nil {
+			panic(err)
+		}
 	},
 }
 
