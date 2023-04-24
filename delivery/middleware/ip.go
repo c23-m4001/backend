ae15620eb3a08d965a9a1678bb6de8c9359fe423
David Yappeter <davidyap11les@gmail.com>
2023-04-24 16:49:20 +0000

    fix: remove logging

--- delivery/middleware/ip.go
+++ delivery/middleware/ip.go
@@ -10,7 +10,6 @@ func IpHandler(router gin.IRouter) {
 	router.Use(func(ctx *gin.Context) {
 		ip := ctx.ClientIP()
 
-		// fmt.Println(ip, ctx.GetHeader("X-Forwarded-For"))
 		ctx.Request = ctx.Request.WithContext(model.SetIpCtx(ctx.Request.Context(), ip))
 		ctx.Next()
 	})
