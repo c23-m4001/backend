8e4abc3d6eef63017b2a639078d914afbb13d515
David Yappeter <davidyap11les@gmail.com>
2023-04-24 16:45:34 +0000

    feat: add logger_stack to infrastructure

---
+++ internal/logger/stdout_logger.go
@@ -0,0 +1,13 @@
+package logger
+
+import "log"
+
+type stdoutLogger struct{}
+
+func (l *stdoutLogger) Write(format string, v ...interface{}) {
+	log.Printf(format+"\n", v...)
+}
+
+func NewStdoutLogger() Logger {
+	return &stdoutLogger{}
+}
