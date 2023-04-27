e68bf9a39dec868dae00b2f6d5e1717a3c6f54da
David Yappeter <davidyap11les@gmail.com>
2023-04-27 07:14:01 +0000

    feat: add baseUseCase

--- use_case/use_case.go
+++ use_case/use_case.go
@@ -1,1 +1,25 @@
 package use_case
+
+import (
+	"capstone/model"
+	"capstone/repository"
+	"context"
+)
+
+type baseUseCase struct {
+	walletRepository repository.WalletRepository
+}
+
+func NewBaseUseCase(
+	walletRepository repository.WalletRepository,
+) *baseUseCase {
+	return &baseUseCase{
+		walletRepository: walletRepository,
+	}
+}
+
+func (u *baseUseCase) mustGetWallet(ctx context.Context, walletId string, isPath bool) model.Wallet {
+	wallet, err := u.walletRepository.Get(ctx, walletId)
+	panicIfRepositoryError(err, "Wallet data not found", isPath)
+	return *wallet
+}
