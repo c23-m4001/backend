f0c48c579e7bd489b58e1a598f17e6873c5ef25d
David Yappeter <davidyap11les@gmail.com>
2023-04-27 07:12:15 +0000

    feat: add dto_request for wallet

---
+++ delivery/dto_request/wallet.go
@@ -0,0 +1,26 @@
+package dto_request
+
+import "capstone/data_type"
+
+type WalletCreateRequest struct {
+	Name     string                   `json:"name" validate:"required,not_empty" example:"Cash"`
+	LogoType data_type.WalletLogoType `json:"logo_type" validate:"required,data_type_enum"`
+} // @name WalletCreateRequest
+
+type WalletFetchSorts []struct {
+	Field     string `json:"field" validate:"required,oneof=name" example:"name"`
+	Direction string `json:"direction" validate:"required,oneof=asc desc" example:"asc"`
+} // @name WalletFetchSorts
+
+type WalletFetchRequest struct {
+	PaginationRequest
+	Sorts  WalletFetchSorts `json:"sorts" validate:"unique=Field,dive"`
+	Phrase *string          `json:"phrase" validate:"omitempty,not_empty" extensions:"x-nullable"`
+} // @name WalletFetchRequest
+
+type WalletUpdateRequest struct {
+	Name     string                   `json:"name" validate:"required,not_empty" example:"Cash"`
+	LogoType data_type.WalletLogoType `json:"logo_type" validate:"required,data_type_enum"`
+
+	WalletId string `json:"-" swaggerignore:"true"`
+} // @name WalletUpdateRequest
