0f492bbde601aed116c8f2716c2c027c4bc3dc2f
David Yappeter <davidyap11les@gmail.com>
2023-04-27 07:10:23 +0000

    feat: add wallet repository to manager

--- manager/repository_manager.go
+++ manager/repository_manager.go
@@ -5,11 +5,13 @@ import "capstone/repository"
 type RepositoryManager interface {
 	UserAccessTokenRepository() repository.UserAccessTokenRepository
 	UserRepository() repository.UserRepository
+	WalletRepository() repository.WalletRepository
 }
 
 type repositoryManager struct {
 	userAccessTokenRepository repository.UserAccessTokenRepository
 	userRepository            repository.UserRepository
+	walletRepository          repository.WalletRepository
 }
 
 func (m *repositoryManager) UserAccessTokenRepository() repository.UserAccessTokenRepository {
@@ -20,10 +22,15 @@ func (m *repositoryManager) UserRepository() repository.UserRepository {
 	return m.userRepository
 }
 
+func (m *repositoryManager) WalletRepository() repository.WalletRepository {
+	return m.walletRepository
+}
+
 func newRepositoryManager(infrastructureManager InfrastructureManager) RepositoryManager {
 	db := infrastructureManager.GetDB()
 	return &repositoryManager{
 		userAccessTokenRepository: repository.NewUserAccessTokenRepository(db),
 		userRepository:            repository.NewUserRepository(db),
+		walletRepository:          repository.NewWalletRepository(db),
 	}
 }
