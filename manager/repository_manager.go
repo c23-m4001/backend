b3ba3c06e1c75c7fc6ccf207cf253d385bedcabc
David Yappeter <davidyap11les@gmail.com>
2023-05-03 03:42:06 +0000

    feat: append category repository to manager

--- manager/repository_manager.go
+++ manager/repository_manager.go
@@ -3,15 +3,21 @@ package manager
 import "capstone/repository"
 
 type RepositoryManager interface {
+	CategoryRepository() repository.CategoryRepository
 	UserAccessTokenRepository() repository.UserAccessTokenRepository
 	UserRepository() repository.UserRepository
 	WalletRepository() repository.WalletRepository
 }
 
 type repositoryManager struct {
+	categoryRepository        repository.CategoryRepository
 	userAccessTokenRepository repository.UserAccessTokenRepository
 	userRepository            repository.UserRepository
 	walletRepository          repository.WalletRepository
+}
+
+func (m *repositoryManager) CategoryRepository() repository.CategoryRepository {
+	return m.categoryRepository
 }
 
 func (m *repositoryManager) UserAccessTokenRepository() repository.UserAccessTokenRepository {
@@ -29,6 +35,7 @@ func (m *repositoryManager) WalletRepository() repository.WalletRepository {
 func newRepositoryManager(infrastructureManager InfrastructureManager) RepositoryManager {
 	db := infrastructureManager.GetDB()
 	return &repositoryManager{
+		categoryRepository:        repository.NewCategoryRepository(db),
 		userAccessTokenRepository: repository.NewUserAccessTokenRepository(db),
 		userRepository:            repository.NewUserRepository(db),
 		walletRepository:          repository.NewWalletRepository(db),
