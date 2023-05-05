71afdfdf670015992560c25ad65685b45a9bc1cc
David Yappeter <davidyap11les@gmail.com>
2023-05-05 01:48:36 +0000

    feat: add transaction migration

---
+++ database/migration/20230503102320_create_transactions_table.go
@@ -0,0 +1,27 @@
+package migration
+
+func init() {
+	sourceDriver.append(
+		20230503102320,
+		`
+			CREATE TABLE IF NOT EXISTS transactions (
+				id char(36) NOT NULL,
+				category_id char(36) NOT NULL,
+				wallet_id char(36) NOT NULL,
+				user_id char(36) NOT NULL,
+				name varchar(255) NOT NULL,
+				amount varchar(255) NOT NULL,
+				date date NOT NULL,
+				created_at timestamp NULL,
+				updated_at timestamp NULL,
+				CONSTRAINT transactions_pk PRIMARY KEY (id),
+				CONSTRAINT transaction_categories_fk FOREIGN KEY (category_id) REFERENCES categories(id),
+				CONSTRAINT transaction_wallets_fk FOREIGN KEY (wallet_id) REFERENCES wallets(id),
+				CONSTRAINT transaction_users_fk FOREIGN KEY (user_id) REFERENCES users(id)
+			);
+		`,
+		`
+			DROP TABLE IF EXISTS transactions;
+		`,
+	)
+}
