052a3edf2a61ce8b8abe992c0e96b9f80962157d
David Yappeter <davidyap11les@gmail.com>
2023-05-08 08:46:46 +0000

    feat: add basic seeder command and user seed

--- database/seeder/production_seeder/production_seeder.go
+++ database/seeder/production_seeder/production_seeder.go
@@ -1,1 +1,28 @@
 package production_seeder
+
+import (
+	"capstone/manager"
+	"fmt"
+)
+
+var Seeders = map[string]func(repositoryManager manager.RepositoryManager){}
+
+func Seed(repositoryManager manager.RepositoryManager, tableName string) {
+	if seed, exist := Seeders[tableName]; exist {
+		seed(repositoryManager)
+	} else {
+		fmt.Printf("Seeder for table `%s` not found\n", tableName)
+	}
+}
+
+func SeedAll(repositoryManager manager.RepositoryManager) {
+	seedOrders := []string{}
+
+	for _, tableName := range seedOrders {
+		seed, ok := Seeders[tableName]
+		if !ok {
+			panic(fmt.Errorf("table name %s not found", tableName))
+		}
+		seed(repositoryManager)
+	}
+}
