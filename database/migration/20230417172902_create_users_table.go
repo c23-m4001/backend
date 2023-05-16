747b7d24d4357c987eb0514257a44f52ecab1b5c
David Yappeter <davidyap11les@gmail.com>
2023-05-16 12:26:24 +0000

    feat: add 'users' table

--- database/migration/20230417172902_create_users_table.go
+++ database/migration/20230417172902_create_users_table.go
@@ -4,8 +4,19 @@ func init() {
 	sourceDriver.append(
 		20230417172902,
 		`
+			CREATE TABLE IF NOT EXISTS users (
+				id char(36) NOT NULL,
+				name varchar(255) NOT NULL,
+				email varchar(255) NOT NULL,
+				password varchar(255) NOT NULL,
+				created_at timestamp NULL,
+				updated_at timestamp NULL,
+				CONSTRAINT users_pk PRIMARY KEY (id),
+				CONSTRAINT users_email_uk UNIQUE (email)
+			);
 		`,
 		`
+			DROP TABLE IF EXISTS users;
 		`,
 	)
 }
