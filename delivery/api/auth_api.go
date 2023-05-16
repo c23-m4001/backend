c1fb83f1a9a7f578c45fd6f4e1c547b0979bbfa7
David Yappeter <davidyap11les@gmail.com>
2023-05-16 19:20:42 +0000

    feat: add register email api

--- delivery/api/auth_api.go
+++ delivery/api/auth_api.go
@@ -28,12 +28,37 @@ func (a *AuthApi) EmailLogin() gin.HandlerFunc {
 			var request dto_request.AuthEmailLoginRequest
 			ctx.mustBind(&request)
 
-			data := a.authUseCase.LoginEmail(ctx.context(), request)
+			token := a.authUseCase.LoginEmail(ctx.context(), request)
 
 			ctx.json(
 				http.StatusOK,
 				dto_response.Response{
-					Data: dto_response.NewAuthTokenResponse(data),
+					Data: dto_response.NewAuthTokenResponse(token),
+				},
+			)
+		},
+	)
+}
+
+//	@Router		/auth/email-register [post]
+//	@Summary	Email Register
+//	@tags		Auth
+//	@Accept		json
+//	@Param		dto_request.AuthEmailRegisterRequest	body	dto_request.AuthEmailRegisterRequest	true	"Body Request"
+//	@Produce	json
+//	@Success	200	{object}	dto_response.Response{data=dto_response.AuthTokenResponse}
+func (a *AuthApi) EmailRegister() gin.HandlerFunc {
+	return a.Guest(
+		func(ctx apiContext) {
+			var request dto_request.AuthEmailRegisterRequest
+			ctx.mustBind(&request)
+
+			token := a.authUseCase.RegisterEmail(ctx.context(), request)
+
+			ctx.json(
+				http.StatusOK,
+				dto_response.Response{
+					Data: dto_response.NewAuthTokenResponse(token),
 				},
 			)
 		},
@@ -49,4 +74,5 @@ func RegisterAuthApi(router gin.IRouter, useCaseManager manager.UseCaseManager) 
 	routerGroup := router.Group("/auth")
 
 	routerGroup.POST("/email-login", api.EmailLogin())
+	routerGroup.POST("/email-register", api.EmailRegister())
 }
