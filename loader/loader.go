87752d66720b4b7b2c258c6b1a973d3b2038fc44
David Yappeter <davidyap11les@gmail.com>
2023-05-16 08:22:12 +0000

    feat: add dataloader constructor

--- loader/loader.go
+++ loader/loader.go
@@ -1,1 +1,15 @@
 package loader
+
+import (
+	"time"
+
+	"github.com/graph-gophers/dataloader"
+)
+
+func newDataloader(batchFn dataloader.BatchFunc) dataloader.Loader {
+	return *dataloader.NewBatchedLoader(
+		batchFn,
+		dataloader.WithWait(10*time.Microsecond),
+		dataloader.WithBatchCapacity(50),
+	)
+}
