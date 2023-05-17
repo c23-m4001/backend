bbb656ea80fb8190854df831e6630b7fafa423d4
David Yappeter <davidyap11les@gmail.com>
2023-05-17 14:02:49 +0000

    feat: dto_request add basic PaginationRequest

--- delivery/dto_request/dto_request.go
+++ delivery/dto_request/dto_request.go
@@ -1,1 +1,6 @@
 package dto_request
+
+type PaginationRequest struct {
+	Page  *int `json:"page" validate:"required_with=Limit,omitempty,gte=1" example:"1" extensions:"x-nullable"`
+	Limit *int `json:"limit" validate:"required_with=Page,omitempty,gte=1,lte=100" example:"100" extensions:"x-nullable"`
+}
