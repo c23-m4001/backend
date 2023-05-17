49b7ccb46cbbc0d47a9073a6478aa0fd94887017
David Yappeter <davidyap11les@gmail.com>
2023-05-17 10:29:30 +0000

    refactor: remove alphanum validation from 'Name'

--- delivery/dto_request/auth.go
+++ delivery/dto_request/auth.go
@@ -6,7 +6,7 @@ type AuthEmailLoginRequest struct {
 } // @name AuthEmailLoginRequest
 
 type AuthEmailRegisterRequest struct {
-	Name     string `json:"name" validate:"required,not_empty,alphanum" example:"John Doe"`
+	Name     string `json:"name" validate:"required,not_empty" example:"John Doe"`
 	Email    string `json:"email" validate:"required,not_empty,email" example:"email@gmail.com"`
 	Password string `json:"password" validate:"required,not_empty,alphanum_symbol" example:"123$#25"`
 } // @name AuthEmailLoginRequest
