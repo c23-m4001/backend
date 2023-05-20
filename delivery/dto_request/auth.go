a8b44a8a1f3cf60cb1f44c9285de66e7cbb8b133
David Yappeter <davidyap11les@gmail.com>
2023-05-20 11:52:49 +0000

    fix: alias naming of dto_request

--- delivery/dto_request/auth.go
+++ delivery/dto_request/auth.go
@@ -9,4 +9,4 @@ type AuthEmailRegisterRequest struct {
 	Name     string `json:"name" validate:"required,not_empty" example:"John Doe"`
 	Email    string `json:"email" validate:"required,not_empty,email" example:"email@gmail.com"`
 	Password string `json:"password" validate:"required,not_empty,alphanum_symbol" example:"123$#25"`
-} // @name AuthEmailLoginRequest
+} // @name AuthEmailRegisterRequest
