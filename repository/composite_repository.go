43e297c760d797c166e41d2e93471a36332e9f08
David Yappeter <davidyap11les@gmail.com>
2023-05-20 14:15:29 +0000

    feat: add DeleteWalletAndTransactions() for composite_repository

--- repository/composite_repository.go
+++ repository/composite_repository.go
@@ -1,10 +1,15 @@
 package repository
 
 import (
+	"capstone/model"
+	"context"
+
 	"github.com/jmoiron/sqlx"
 )
 
 type CompositeRepository interface {
+	// wallet
+	DeleteWalletAndTransactions(ctx context.Context, wallet *model.Wallet) error
 }
 
 type compositeRepository struct {
@@ -17,4 +22,26 @@ func NewCompositeRepository(
 	return &compositeRepository{
 		db: db,
 	}
+}
+
+func (r *compositeRepository) DeleteWalletAndTransactions(ctx context.Context, wallet *model.Wallet) error {
+	tx, err := r.db.Beginx()
+	if err != nil {
+		return translateSqlError(err)
+	}
+
+	walletRepository := NewWalletRepository(tx)
+	transactionRepository := NewTransactionRepository(tx)
+
+	if err := transactionRepository.DeleteByWalletId(ctx, wallet.Id); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := walletRepository.Delete(ctx, wallet); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return translateSqlError(tx.Commit())
 }
