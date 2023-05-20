d859c65b21dcf4db9e02c2ace2ff18a5225dd82b
David Yappeter <davidyap11les@gmail.com>
2023-05-20 14:20:29 +0000

    feat: append composite repository to manager

--- manager/repository_manager.go
+++ manager/repository_manager.go
@@ -4,6 +4,7 @@ import "capstone/repository"
 
 type RepositoryManager interface {
 	CategoryRepository() repository.CategoryRepository
+	CompositeRepository() repository.CompositeRepository
 	TransactionRepository() repository.TransactionRepository
 	UserAccessTokenRepository() repository.UserAccessTokenRepository
 	UserRepository() repository.UserRepository
@@ -12,6 +13,7 @@ type RepositoryManager interface {
 
 type repositoryManager struct {
 	categoryRepository        repository.CategoryRepository
+	compositeRepository       repository.CompositeRepository
 	transactionRepository     repository.TransactionRepository
 	userAccessTokenRepository repository.UserAccessTokenRepository
 	userRepository            repository.UserRepository
@@ -20,6 +22,10 @@ type repositoryManager struct {
 
 func (m *repositoryManager) CategoryRepository() repository.CategoryRepository {
 	return m.categoryRepository
+}
+
+func (m *repositoryManager) CompositeRepository() repository.CompositeRepository {
+	return m.compositeRepository
 }
 
 func (m *repositoryManager) TransactionRepository() repository.TransactionRepository {
@@ -42,6 +48,7 @@ func newRepositoryManager(infrastructureManager InfrastructureManager) Repositor
 	db := infrastructureManager.GetDB()
 	return &repositoryManager{
 		categoryRepository:        repository.NewCategoryRepository(db),
+		compositeRepository:       repository.NewCompositeRepository(db),
 		transactionRepository:     repository.NewTransactionRepository(db),
 		userAccessTokenRepository: repository.NewUserAccessTokenRepository(db),
 		userRepository:            repository.NewUserRepository(db),
