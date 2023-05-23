b812ac809932bd4569b9a7a84ae14fbf283aa04a
David Yappeter <davidyap11les@gmail.com>
2023-05-23 18:15:52 +0000

    feat: add logo_type on category dto_response

--- delivery/dto_response/category.go
+++ delivery/dto_response/category.go
@@ -1,14 +1,16 @@
 package dto_response
 
 import (
+	"capstone/data_type"
 	"capstone/model"
 )
 
 type CategoryResponse struct {
-	Id        string `json:"id" example:"023b6735-8255-43c0-bc3d-f6d1e423612d"`
-	Name      string `json:"name" example:"Makanan"`
-	IsGlobal  bool   `json:"is_global"`
-	IsExpense bool   `json:"is_expense"`
+	Id        string                     `json:"id" example:"023b6735-8255-43c0-bc3d-f6d1e423612d"`
+	Name      string                     `json:"name" example:"Makanan"`
+	IsGlobal  bool                       `json:"is_global"`
+	IsExpense bool                       `json:"is_expense"`
+	LogoType  data_type.CategoryLogoType `json:"logo_type"`
 	Timestamp
 } // @name CategoryResponse
 
@@ -18,6 +20,7 @@ func NewCategoryResponse(category model.Category) CategoryResponse {
 		Name:      category.Name,
 		IsGlobal:  category.IsGlobal,
 		IsExpense: category.IsExpense,
+		LogoType:  category.LogoType,
 		Timestamp: Timestamp(category.Timestamp),
 	}
 	return r
